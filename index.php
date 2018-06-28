e324c7bacc0614abba5bbe4def035d0759eaf109
Joaotecnologia <jjrjoaozinho@hotmail.com>
2018-06-28 20:22:38 -0300

    Corrig

--- index.php
+++ index.php
@@ -1,15 +1,7 @@
 <?php
 
-<<<<<<< HEAD
   echo "Olá Mundo 452!";
 
   echo "Ola ";
-=======
-  echo "Olá Mundo!";
- 
-  echo "Olá Cidade!";
-
->>>>>>> 27cf37024f086ffb2e69ec414175152eda88e4ae
-
 
 ?>
